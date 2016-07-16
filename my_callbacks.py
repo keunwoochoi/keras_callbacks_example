b49de938ce84b569ab39b9f00ac87f4dc5df4f82
keunwoochoi <gnuchoi+github@gmail.com>
2016-07-15 22:36:37 -0300

    add python files

---
+++ my_callbacks.py
@@ -0,0 +1,25 @@
+import keras
+from sklearn.metrics import roc_auc_score
+
+class Histories(keras.callbacks.Callback):
+	def on_train_begin(self, logs={}):
+		self.aucs = []
+		self.losses = []
+
+	def on_train_end(self, logs={}):
+		return
+
+	def on_epoch_begin(self, epoch, logs={}):
+		return
+
+	def on_epoch_end(self, epoch, logs={}):
+		self.losses.append(logs.get('loss'))
+		y_pred = self.model.predict(self.model.validation_data[0])
+		self.aucs.append(roc_auc_score(self.model.validation_data[1], y_pred))
+		return
+
+	def on_batch_begin(self, batch, logs={}):
+		return
+
+	def on_batch_end(self, batch, logs={}):
+		return
